4614428d32cf99f961ac230095b5af004166a0fc
John Hill <jphill1989@gmail.com>
2024-10-30 14:17:03 -0600

    Created mock data for our Home component

--- src/router/home/home.component.jsx
+++ src/router/home/home.component.jsx
@@ -1,5 +1,63 @@
+import { useState } from "react";
+
+
+const data = {
+  "data": [
+    {
+      "title": "Recipe 1",
+      "servingSize": 1,
+      "ingredients": [
+        {
+          "name": "Ingredient Button",
+          "price": "$$"
+        },
+        {
+          "name": "Ingredient Button",
+          "price": "$$"
+        },
+        {
+          "name": "Ingredient Button",
+          "price": "$$"
+        },
+        {
+          "name": "Ingredient Button",
+          "price": "00"
+        }
+      ],
+      "image": {
+        "src": "image-url-1",
+        "alt": "Recipe 1 Image"
+      },
+  
+    },
+    {
+      "title": "Recipe 2",
+      "servingSize": 4,
+      "ingredients": [
+        {
+          "name": "Ingredient Button",
+          "price": "$$"
+        },
+        {
+          "name": "Ingredient Button",
+          "price": "$$"
+        },
+        {
+          "name": "Ingredient Button",
+          "price": "$$"
+        }
+      ],
+      "image": {
+        "src": "image-url-2",
+        "alt": "Recipe 2 Image"
+      },
+
+    }
+  ]
+}
 
 function Home() {
+  const [ indexData, setIndexData] = useState(data)
   return (
     <div></div>
   );
