e3cd7a5a72e0c1da410a1b12a73cc86af61d18b1
Charles B. <charlesbanks238@gmail.com>
2024-10-30 17:59:32 -0700

    Added: RecipeCard component and some initial styling. left commented out options for horizantal and vertical orientations. Also added 3 font importations from google fonts in the index HTML file.

---
+++ src/component/RecipeCard/RecipeCard.jsx
@@ -0,0 +1,63 @@
+import './RecipeCard.css';
+
+export function RecipeCard ({ recipe }) {
+  const {title, servingSize, ingredients, image} = recipe;
+
+  // The arrangment below is for the HORIZONTAL ORIENTATION of the the recipe card
+
+  return (
+    <div className={'recipe-card-wrap'} alt={`Recipe card for ${ title }`} tabindex='0'>
+      <div className={'recipe-ingredient-and-image-display'} >
+        <div className={ 'ingredients-box' }>
+          {
+            ingredients.map((ingredient) => {
+              return (
+                <div className={ `${ ingredient.name }-button ingredient-button-wrapper` }>
+                  <button className={`button-element`}>{ ingredient.name }<span> {ingredient.price} </span></button>
+                </div>
+              )
+            })
+          }
+        </div>
+        <div className={ 'recipe-image-contianer' }>
+          <img className={ 'recipe-image-actual' } src={ image.src } alt={ `${ image.alt }` } />
+        </div>
+      </div>
+      <div className={ 'reipe-card-summary-bar' }>
+        <p className={ 'total-cost' }></p>
+        <p className={ 'recipe-title' }>{ `${title}` }</p>
+        <p className={ 'seving-size' }>{ `Servings: ${servingSize}` }</p>
+      </div>
+    </div>
+  )
+
+  // The arrangment below is for the VERTICAL ORIENTATION of the the recipe card
+
+  // return (
+  //   <div className={'recipe-card-wrap'} alt={`Recipe card for ${ title }`} tabindex='0'>
+  //     <div className={'recipe-ingredient-and-image-display'} >
+  //       <div className={ 'ingredients-box' }>
+  //         {
+  //           ingredients.map((ingredient) => {
+  //             return (
+  //               <div className={ `${ ingredient.name }-button ingredient-button-wrapper` }>
+  //                 <button className={`button-element`}>{ ingredient.name }<span> {ingredient.price} </span></button>
+  //               </div>
+  //             )
+  //           })
+  //         }
+  //       </div>
+  //       <div className={ 'recipe-image-contianer' }>
+  //         <img className={ 'recipe-image-actual' } src={ image.src } alt={ `${ image.alt }` } />
+  //       </div>
+  //     </div>
+  //     <div className={ 'reipe-card-summary-bar' }>
+  //       <p className={ 'total-cost' }></p>
+  //       <p className={ 'recipe-title' }>{ `${title}` }</p>
+  //       <p className={ 'seving-size' }>{ `Servings: ${servingSize}` }</p>
+  //     </div>
+  //   </div>
+  // )
+}
+
+export default RecipeCard;
