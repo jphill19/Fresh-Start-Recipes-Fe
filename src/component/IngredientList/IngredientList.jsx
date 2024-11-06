3e122c01326c39022b6c9cc9fc82c838e1f64779
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-11-06 10:17:24 -0700

    Add total cost to bottom of Ingredient Container for RecipePage

--- src/component/IngredientList/IngredientList.jsx
+++ src/component/IngredientList/IngredientList.jsx
@@ -3,6 +3,10 @@ import './IngredientList.css';
 function IngredientList({ ingredients }) {
   console.log('ingredients: ', ingredients)
 
+  const totalCost = ingredients.reduce((total, ingredient) => {
+    return total + ingredient.price;
+  }, 0);
+
   return (
     <div className="ingredients-container">
       <h2>Ingredient List</h2>
@@ -11,10 +15,13 @@ function IngredientList({ ingredients }) {
           <div key={index} className="individual-ingredient">
             <p>{ingredient.ingredient}</p>
             <p>{ingredient.quantity} {ingredient.measurement}</p>
-            <p>${ingredient.price}</p>
+            <p>${ingredient.price.toFixed(2)}</p>
           </div>
         ))
       }
+      <div className="total-price">
+        <h3>Total Cost: ${totalCost.toFixed(2)}</h3>
+      </div>
     </div>
   );
 }
