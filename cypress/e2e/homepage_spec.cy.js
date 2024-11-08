5bd5fee0e2fabff25ed7566d50a0b94922dcf345
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-11-07 21:53:25 -0700

    WIP Serving size filter tests

--- cypress/e2e/homepage_spec.cy.js
+++ cypress/e2e/homepage_spec.cy.js
@@ -11,7 +11,7 @@ describe('Home Page', () => {
     cy.wait('@recipe-data')
   })
   
-  // it.skip('DISPLAYS all elements on the home page', () => {
+  // it('DISPLAYS all elements on the home page', () => {
   //   cy.get('.header-section').should('exist')
   //   .get('.location-icon').should('exist')
   //   .get('.location-icon').should('be.visible')
@@ -27,7 +27,7 @@ describe('Home Page', () => {
   //   .get('.filter-bar-container > :nth-child(3)').should('contain', 'Servings')
   //   .get('.filter-bar-container > :nth-child(4)').should('contain', 'Price')
   //   .get('.recipes-container').should('exist')
-  //   .get('.recipes-container').children().should('have.length', 2)
+  //   .get('.recipes-container').children().should('have.length', 3)
   //   .get('.recipes-container > :nth-child(1) > .recipe-link > .recipe-title').should('contain', 'Baked Potato')
   //   .get('.recipes-container > :nth-child(1) > .recipe-link > .recipe-image').should('exist')
   //   .get('.recipes-container > :nth-child(1) > .ingredients-box').children().should('have.length', 3)
@@ -40,7 +40,9 @@ describe('Home Page', () => {
   //   .get('.recipes-container > :nth-child(1) > .recipe-card-footer').should('exist')
   //   .get('.recipes-container > :nth-child(1) > .recipe-card-footer > .serving-size').should('contain', 'Servings: 1')
   //   .get('.recipes-container > :nth-child(1) > .recipe-card-footer > .total-cost').should('contain', 'Total Cost: $7.00')
-
+  //   // I think we might be able to clean these up.
+  //   // I don't know if we need to access all of these elements from the top down like this
+  //   // And they'll run a lot faster
   //   .get('.recipes-container > :nth-child(2) > .recipe-link > .recipe-title').should('contain', 'Hamburger Potato Soup')
   //   .get('.recipes-container > :nth-child(2) > .recipe-link > .recipe-image').should('exist')
   //   .get('.recipes-container > :nth-child(2) > .ingredients-box').children().should('have.length', 4)
@@ -71,40 +73,235 @@ describe('Home Page', () => {
   //   // .get('.filter-bar-container > :nth-child(1)').click()
   // })
 
-  it('DISPLAYS price filter modal upon clicking dropdown', () => {
+  // PASSING
+  // it('DISPLAYS price filter modal upon clicking dropdown', () => {
+  //   cy.get('.modal-content').should('not.exist')
+  //   .get('.modal-backdrop').should('not.exist')
+  //   .get(':nth-child(4) > .dropdown-arrow').click()
+  //   // Open modal
+  //   .get('.modal-backdrop').should('exist')
+  //   .get('.modal-content').should('be.visible')
+  //   // Check contents of modal
+  //   .get('.modal-content > h2').should('contain', 'Filter by Prices')
+    
+  //   // Checkboxes
+  //   .get(':nth-child(2)').should('be.visible')
+  //   .get(':nth-child(2) > input').should('be.visible')
+  //   // Change to 'Less than $5' later
+  //   .get(':nth-child(2)').should('contain', 'Greater than $5')
+
+  //   .get(':nth-child(3)').should('be.visible')
+  //   .get(':nth-child(3) > input').should('be.visible')
+  //   // .get(':nth-child(3) > input').should('not.be.checked')
+  //   .get(':nth-child(3)').should('contain', 'Less than $10')
+
+  //   .get(':nth-child(4)').should('be.visible')
+  //   .get(':nth-child(4) > input').should('be.visible')
+  //   // .get(':nth-child(4) > input').should('not.be.checked')
+  //   .get(':nth-child(4)').should('contain', 'Greater than $10')
+  //   .get('.reset').should('be.visible')
+  //   .get('.view-results').should('be.visible')
+  // })
+
+  // // PASSING
+  // // This needs the logic updated
+  // // Test is written for when it's been fixed
+  // it('FILTERS recipes Less than $5', () => {
+  //   cy.url().should('eq', 'http://localhost:3001/')
+
+  //   cy.get(':nth-child(4) > .dropdown-arrow').click()
+
+  //   .get('.modal-content').should('be.visible')
+  //   .get('.modal-backdrop').should('be.visible')
+
+  //   .get(':nth-child(2) > input').should('not.be.checked')
+  //   .get(':nth-child(2) > input').click()
+  //   .get('.view-results').click()
+
+  //   .get('.modal-content').should('not.exist')
+  //   .get('.modal-backdrop').should('not.exist')
+
+  //   // Change to .should('contain', 'Cup of Dirt)
+  //   .get('.recipe-title').should('contain', 'Baked Potato')
+  //   cy.url().should('eq', 'http://localhost:3001/?by_price=1')
+  // })
+
+  // // PASSING
+  // it('FILTERS recipes Less than $10', () => {
+  //   cy.url().should('eq', 'http://localhost:3001/')
+
+  //   cy.get(':nth-child(4) > .dropdown-arrow').click()
+
+  //   .get('.modal-content').should('be.visible')
+  //   .get('.modal-backdrop').should('be.visible')
+
+  //   .get(':nth-child(3) > input').should('not.be.checked')
+  //   .get(':nth-child(3) > input').click()
+  //   .get('.view-results').click()
+
+  //   .get('.modal-content').should('not.exist')
+  //   .get('.modal-backdrop').should('not.exist')
+
+  //   // Change to .should('contain', 'Cup of Dirt)
+  //   .get('.recipe-title').should('contain', 'Baked Potato')
+  //   cy.url().should('eq', 'http://localhost:3001/?by_price=2')
+  // })
+
+  // // NOT PASSING
+  // // ARE WE NOT HITTING THE FIXTURE FILE?
+  // // PRICES AREN'T CORRECT FROM DATA
+  // it('FILTERS recipes Greater than $10', () => {
+  //   cy.url().should('eq', 'http://localhost:3001/')
+
+  //   cy.get(':nth-child(4) > .dropdown-arrow').click()
+
+  //   .get('.modal-content').should('be.visible')
+  //   .get('.modal-backdrop').should('be.visible')
+
+  //   .get(':nth-child(4) > input').should('not.be.checked')
+  //   .get(':nth-child(4) > input').click()
+  //   .get('.view-results').click()
+
+  //   .get('.modal-content').should('not.exist')
+  //   .get('.modal-backdrop').should('not.exist')
+
+  //   .get('.recipe-title').should('contain', 'Hamburger Potato Soup')
+  //   cy.url().should('eq', 'http://localhost:3001/?by_price=3')
+  // })
+
+    // PASSING
+    // it('RESETS the filter for Less than $5', () => {
+    
+    //   //Clicking Reset from modal
+    //   cy.get(':nth-child(4) > .dropdown-arrow').click()
+
+    //   .get(':nth-child(2) > input').click()
+    //   .get('.reset').click()
+
+    //   .get('.modal-content').should('not.exist')
+    //   .get('.modal-backdrop').should('not.exist')
+
+    //   .get('.recipes-container').children().should('have.length', 3)
+    
+    //   // Clicking blacked out Price filter button
+    //   .get(':nth-child(4) > .dropdown-arrow').click()
+
+    //   .get(':nth-child(2) > input').click()
+    //   .get('.view-results').click()
+
+    //   .get('.modal-content').should('not.exist')
+    //   .get('.modal-backdrop').should('not.exist')
+
+    //   // Can't seem to target the blacked out Price filter button
+    //   .get(':nth-child(4) > .x-icon').click()
+
+    //   .get('.recipes-container').children().should('have.length', 3)
+    // })
+
+    // PASSING
+    // it('RESETS the filter for Less than $10', () => {
+    
+    //   //Clicking Reset from modal
+    //   cy.get(':nth-child(4) > .dropdown-arrow').click()
+
+    //   .get(':nth-child(3) > input').click()
+    //   .get('.reset').click()
+
+    //   .get('.modal-content').should('not.exist')
+    //   .get('.modal-backdrop').should('not.exist')
+
+    //   .get('.recipes-container').children().should('have.length', 3)
+    
+    //   // Clicking blacked out Price filter button
+    //   .get(':nth-child(4) > .dropdown-arrow').click()
+
+    //   .get(':nth-child(3) > input').click()
+    //   .get('.view-results').click()
+
+    //   .get('.modal-content').should('not.exist')
+    //   .get('.modal-backdrop').should('not.exist')
+
+    //   // Can't seem to target the blacked out Price filter button
+    //   .get(':nth-child(4) > .x-icon').click()
+
+    //   .get('.recipes-container').children().should('have.length', 3)
+    // })
+
+    // PASSING
+    // it('RESETS the filter for Greater than $10', () => {
+    
+    //   //Clicking Reset from modal
+    //   cy.get(':nth-child(4) > .dropdown-arrow').click()
+
+    //   .get(':nth-child(4) > input').click()
+    //   .get('.reset').click()
+
+    //   .get('.modal-content').should('not.exist')
+    //   .get('.modal-backdrop').should('not.exist')
+
+    //   .get('.recipes-container').children().should('have.length', 3)
+    
+    //   // Clicking blacked out Price filter button
+    //   .get(':nth-child(4) > .dropdown-arrow').click()
+
+    //   .get(':nth-child(4) > input').click()
+    //   .get('.view-results').click()
+
+    //   .get('.modal-content').should('not.exist')
+    //   .get('.modal-backdrop').should('not.exist')
+
+    //   // Can't seem to target the blacked out Price filter button
+    //   .get(':nth-child(4) > .x-icon').click()
+
+    //   .get('.recipes-container').children().should('have.length', 3)
+    // })
+
+
+  
+  it('DISPLAYS serving size filter modal upon clicking dropdown', () => {
     cy.get('.modal-content').should('not.exist')
     .get('.modal-backdrop').should('not.exist')
-    .get(':nth-child(4) > .dropdown-arrow').click()
-    // Open modal
+    .get(':nth-child(3) > .dropdown-arrow').click()
+
     .get('.modal-backdrop').should('exist')
     .get('.modal-content').should('be.visible')
-    // Check contents of modal
-    .get('.modal-content > h2').should('be.visible')
-    .get('.modal-content > h2').should('contain', 'Filter by Prices')
-    .get('.modal-content > :nth-child(2)').should('be.visible')
-    // Change to 'Less than $5' later
-    .get('.modal-content > :nth-child(2) > input').should('be.visible')
-    .get('.modal-content > :nth-child(2)').should('contain', 'Greater than $5')
-    .get(':nth-child(2) > input').should('be.visible')
-    .get('.modal-content > :nth-child(3)').should('be.visible')
-    .get('.modal-content > :nth-child(3)').should('contain', 'Less than $10')
-    .get(':nth-child(3) > input').should('be.visible')
-    .get('.modal-content > :nth-child(2) > input').should('be.visible')
-    .get('.modal-content > :nth-child(4)').should('be.visible')
-    .get('.modal-content > :nth-child(4)').should('contain', 'Greater than $10')
-    .get(':nth-child(4) > input').should('be.visible')
-    .get('.modal-content > :nth-child(2) > input').should('be.visible')
-    // .get('')
+
+    .get('.modal-content > h2').should('contain', 'Filter by Servings')
+    
+    .get('.toggle-switch').should('be.visible')
+    .get('.toggle-option.active').should('contain', 'Single')
+    .get('.toggle-option').should('contain', 'Multiple')
+    .get('.toggle-option.active').should('contain', 'Single').click()
+    .get('.toggle-option').should('contain', 'Single')
+    .get('.toggle-option.active').should('contain', 'Multiple')
+    .get('.toggle-option.active').should('contain', 'Multiple').click()
+    .get('.reset').should('be.visible')
+    .get('.view-results').should('be.visible')    
   })
+  
+  // // UPDATE BE DATA AND THIS WILL PASS
+  // it('FILTERS reci MULTIPLE serving size', () => {
+  //   cy.get(':nth-child(3) > .dropdown-arrow').click()
+  //   .get('.toggle-option.active').should('contain', 'Single').click()
+  //   .get('.view-results').click()
 
-  it('FILTERS recipes less than $5', () => {
-    cy.get(':nth-child(4) > .dropdown-arrow').click();
+  //   cy.url().should('eq', 'http://localhost:3001/?by_serving=Multiple')
+  //   .get('.recipes-container').children().should('have.length', 2)
+  // })
 
-    cy.get('.modal-content').should('be.visible');
-    cy.get('.modal-backdrop').should('be.visible');
+  // UPDATE BE DATA AND THIS WILL PASS
+  it('FILTERS recipes on SINGLE serving size', () => {
+    cy.get(':nth-child(3) > .dropdown-arrow').click()
+    .get('.view-results').click()
+
+    cy.url().should('eq', 'http://localhost:3001/?by_serving=Single')
+    .get('.recipes-container').children().should('have.length', 1)
   })
-
 })
+
+// NEED:
+// SHOW RESET JUST LIKE ABOVE FOR PRICE
+
 
 
 
