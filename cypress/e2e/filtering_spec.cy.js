f4e3d9f37065cde2057ab043faeaf326ee7e7775
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-11-08 19:24:40 -0700

    Tests passing filtering_spec.cy.js

--- cypress/e2e/filtering_spec.cy.js
+++ cypress/e2e/filtering_spec.cy.js
@@ -9,7 +9,6 @@ describe('Filtering Functionality', () => {
     cy.wait('@recipe-data');
   });
   
-  // PASSING
   it('FILTERS recipes Less than $5', () => {
     cy.intercept('GET', 'https://whispering-thicket-76959-66145e05673c.herokuapp.com/api/v1/recipes?by_price=0', {
       statusCode: 200,
@@ -27,105 +26,117 @@ describe('Filtering Functionality', () => {
       .url().should('eq', 'http://localhost:3001/?by_price=0')
   });
 
-  // // PASSING
-  // it('FILTERS recipes Less than $10', () => {
-  //   cy.intercept('GET', 'https://whispering-thicket-76959-66145e05673c.herokuapp.com/api/v1/recipes?by_price=2', {
-  //     statusCode: 200,
-  //     fixture: 'filter-less-than-10'
-  //   });
+  it('FILTERS recipes Less than $10', () => {
+    cy.intercept('GET', 'https://whispering-thicket-76959-66145e05673c.herokuapp.com/api/v1/recipes?by_price=2', {
+      statusCode: 200,
+      fixture: 'filter-less-than-10'
+    });
 
-  //   cy.get(':nth-child(4) > .dropdown-arrow').click()
-  //     .get('.modal-content').should('be.visible')
-  //     .get('.modal-backdrop').should('be.visible')
-  //     .get(':nth-child(3) > input').should('not.be.checked').click()
-  //     .get('.view-results').click()
-  //     .get('.modal-content').should('not.exist')
-  //     .get('.modal-backdrop').should('not.exist')
-  //     .get('.recipes-container').children().should('have.length', 5)
-  //     .url().should('eq', 'http://localhost:3001/?by_price=2');
-  // });
+    cy.get(':nth-child(4) > .dropdown-arrow').click()
+      .get('.modal-content').should('be.visible')
+      .get('.modal-backdrop').should('be.visible')
+      .get(':nth-child(3) > input').should('not.be.checked').click()
+      .get('.view-results').click()
+      .get('.modal-content').should('not.exist')
+      .get('.modal-backdrop').should('not.exist')
+      .get('.recipes-container').children().should('have.length', 5)
+      .url().should('eq', 'http://localhost:3001/?by_price=2');
+  });
 
-  // //  PASSING
-  // it('FILTERS recipes Greater than $10', () => {      
-  //   cy.intercept('GET', 'https://whispering-thicket-76959-66145e05673c.herokuapp.com/api/v1/recipes?by_price=3', {
-  //     statusCode: 200,
-  //     fixture: 'filter-greater-than-10'
-  //   });
+  //  PASSING
+  it('FILTERS recipes Greater than $10', () => {      
+    cy.intercept('GET', 'https://whispering-thicket-76959-66145e05673c.herokuapp.com/api/v1/recipes?by_price=3', {
+      statusCode: 200,
+      fixture: 'filter-greater-than-10'
+    });
 
-  //   cy.get(':nth-child(4) > .dropdown-arrow').click()
-  //     .get('.modal-content').should('be.visible')
-  //     .get('.modal-backdrop').should('be.visible')
-  //     .get(':nth-child(4) > input').should('not.be.checked').click()
-  //     .get('.view-results').click()
-  //     .get('.modal-content').should('not.exist')
-  //     .get('.modal-backdrop').should('not.exist')
-  //     .get('.recipes-container').children().should('have.length', 5)
-  //     .url().should('eq', 'http://localhost:3001/?by_price=3');
-  // });
+    cy.get(':nth-child(4) > .dropdown-arrow').click()
+      .get('.modal-content').should('be.visible')
+      .get('.modal-backdrop').should('be.visible')
+      .get(':nth-child(4) > input').should('not.be.checked').click()
+      .get('.view-results').click()
+      .get('.modal-content').should('not.exist')
+      .get('.modal-backdrop').should('not.exist')
+      .get('.recipes-container').children().should('have.length', 5)
+      .url().should('eq', 'http://localhost:3001/?by_price=3');
+  });
 
-  // // PASSING
-  // it('RESETS the filter for Less than $5', () => {
-  //   cy.get(':nth-child(4) > .dropdown-arrow').click()
-  //     .get(':nth-child(2) > input').click()
-  //     .get('.reset').click()
-  //     .get('.modal-content').should('not.exist')
-  //     .get('.modal-backdrop').should('not.exist')
-  //     .get('.recipes-container').children().should('have.length', 10);
-  // });
+  it('CLEARS the filter & closes modal for Less than $5', () => {
+    cy.get(':nth-child(4) > .dropdown-arrow').click()
+      .get(':nth-child(2) > input').click()
+      .get('.reset').click()
+      .get('.modal-content').should('not.exist')
+      .get('.modal-backdrop').should('not.exist')
+      .get('.recipes-container').children().should('have.length', 10);
+  });
 
-  // // PASSING
-  // it('RESETS the filter for Less than $10', () => {
-  //   cy.get(':nth-child(4) > .dropdown-arrow').click()
-  //     .get(':nth-child(3) > input').click()
-  //     .get('.reset').click()
-  //     .get('.modal-content').should('not.exist')
-  //     .get('.modal-backdrop').should('not.exist')
-  //     .get('.recipes-container').children().should('have.length', 10);
-  // });
+  it('CLEARS the filter & closes modal for Less than $10', () => {
+    cy.get(':nth-child(4) > .dropdown-arrow').click()
+      .get(':nth-child(3) > input').click()
+      .get('.reset').click()
+      .get('.modal-content').should('not.exist')
+      .get('.modal-backdrop').should('not.exist')
+      .get('.recipes-container').children().should('have.length', 10);
+  });
 
-  // // PASSING
-  // it('RESETS the filter for Greater than $10', () => {
-  //   cy.get(':nth-child(4) > .dropdown-arrow').click()
-  //     .get(':nth-child(4) > input').click()
-  //     .get('.reset').click()
-  //     .get('.modal-content').should('not.exist')
-  //     .get('.modal-backdrop').should('not.exist')
-  //     .get('.recipes-container').children().should('have.length', 10);
-  // });
+  it('CLEARS the filter & closes modal for Greater than $10', () => {
+    cy.get(':nth-child(4) > .dropdown-arrow').click()
+      .get(':nth-child(4) > input').click()
+      .get('.reset').click()
+      .get('.modal-content').should('not.exist')
+      .get('.modal-backdrop').should('not.exist')
+      .get('.recipes-container').children().should('have.length', 10);
+  });
 
-  // // UPDATE BE DATA AND THIS WILL PASS
-  // it('FILTERS recipes - MULTIPLE serving size', () => {
-  //   cy.get(':nth-child(3) > .dropdown-arrow').click()
-  //     .get('.toggle-option.active').should('contain', 'Single').click()
-  //     .get('.view-results').click()
-  //     .url().should('eq', 'http://localhost:3001/?by_serving=Multiple')
-  //     .get('.recipes-container').children().should('have.length', 2);
-  // });
+  it('FILTERS recipes - MULTIPLE serving size', () => {
+    cy.intercept('GET', 'https://whispering-thicket-76959-66145e05673c.herokuapp.com/api/v1/recipes?by_serving=Multiple', {
+      statusCode: 200,
+      fixture: 'filter-serving-multiple'
+    });
 
-  // // UPDATE BE DATA AND THIS WILL PASS
-  // it('FILTERS recipes - SINGLE serving size', () => {
-  //   cy.get(':nth-child(3) > .dropdown-arrow').click()
-  //     .get('.view-results').click()
-  //     .url().should('eq', 'http://localhost:3001/?by_serving=Single')
-  //     .get('.recipes-container').children().should('have.length', 1);
-  // });
+    cy.get(':nth-child(3) > .dropdown-arrow').click()
+      .get('.toggle-option.active').should('contain', 'Single').click()
+      .get('.view-results').click()
+      .url().should('eq', 'http://localhost:3001/?by_serving=Multiple')
+      .get('.recipes-container').children().should('have.length', 8);
+  });
 
-  // // UPDATE BE DATA AND THIS WILL PASS
-  // it('RESETS Serving Size Filter from modal - SINGLE', () => {
-  //   cy.get(':nth-child(3) > .dropdown-arrow').click()
-  //     .get('.view-results').click()
-  //     .url().should('eq', 'http://localhost:3001/?by_serving=Single')
-  //     .get(':nth-child(3) > .x-icon').click()
-  //     .get('.recipes-container').children().should('have.length', 1);
-  // });
+  it('FILTERS recipes - SINGLE serving size', () => {
+    cy.intercept('GET', 'https://whispering-thicket-76959-66145e05673c.herokuapp.com/api/v1/recipes?by_serving=Single', {
+      statusCode: 200,
+      fixture: 'filter-serving-single'
+    });
+    
+    cy.get(':nth-child(3) > .dropdown-arrow').click()
+      .get('.view-results').click()
+      .url().should('eq', 'http://localhost:3001/?by_serving=Single')
+      .get('.recipes-container').children().should('have.length', 2);
+  });
 
-  // // UPDATE BE DATA AND THIS WILL PASS
-  // it('RESETS Serving Size Filter from modal - MULTIPLE', () => {
-  //   cy.get(':nth-child(3) > .dropdown-arrow').click()
-  //     .get('.toggle-option.active').should('contain', 'Single').click()
-  //     .get('.view-results').click()
-  //     .url().should('eq', 'http://localhost:3001/?by_serving=Multiple')
-  //     .get(':nth-child(3) > .x-icon').click()
-  //     .get('.recipes-container').children().should('have.length', 2);
-  // });
+  it('CLEARS Serving Size Filter & closes modal - SINGLE', () => {
+    cy.intercept('GET', 'https://whispering-thicket-76959-66145e05673c.herokuapp.com/api/v1/recipes?by_serving=Single', {
+      statusCode: 200,
+      fixture: 'filter-serving-single'
+    });
+
+    cy.get(':nth-child(3) > .dropdown-arrow').click()
+      .get('.view-results').click()
+      .url().should('eq', 'http://localhost:3001/?by_serving=Single')
+      .get(':nth-child(3) > .x-icon').click()
+      .get('.recipes-container').children().should('have.length', 10);
+  });
+
+  it('CLEARS Serving Size Filter & closes modal - MULTIPLE', () => {
+    cy.intercept('GET', 'https://whispering-thicket-76959-66145e05673c.herokuapp.com/api/v1/recipes?by_serving=Multiple', {
+      statusCode: 200,
+      fixture: 'filter-serving-multiple'
+    });
+
+    cy.get(':nth-child(3) > .dropdown-arrow').click()
+      .get('.toggle-option.active').should('contain', 'Single').click()
+      .get('.view-results').click()
+      .url().should('eq', 'http://localhost:3001/?by_serving=Multiple')
+      .get(':nth-child(3) > .x-icon').click()
+      .get('.recipes-container').children().should('have.length', 10);
+  });
 });
