8567d98012b877fa450879229d0b01e00642f730
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-11-09 10:09:49 -0700

    All tests intercepted & passing

---
+++ cypress/e2e/sad_path_spec.cy.js
@@ -0,0 +1,13 @@
+describe('Filtering Functionality', () => {
+  it('returns an empty array when the BE has no data', () => {
+    cy.intercept('GET', 'https://whispering-thicket-76959-66145e05673c.herokuapp.com/api/v1/recipes?', {
+      statusCode: 200,
+      fixture: 'no-data'
+    }).as('no-data');
+    
+    cy.visit('http://localhost:3001/');
+    cy.wait('@no-data');
+    
+    cy.get('.recipes-container').should('contain', 'No recipes available.')
+  })  
+})
