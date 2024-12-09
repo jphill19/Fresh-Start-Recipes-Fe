34e8ffdb9cef072e8cadea3a96a986da34364ea4
John Hill <jphill1989@gmail.com>
2024-12-08 17:30:52 -0700

    fixed the build

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -24,6 +24,11 @@ self.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
+    ],
+    "/recipe/[recipeId]": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/recipe/[recipeId].js"
     ]
   },
   "ampFirstPages": []
